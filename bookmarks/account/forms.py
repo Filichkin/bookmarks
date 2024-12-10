916cb418fca75f2773f40c4b41f2639400800fb1
Alexey Filichkin <filichkin.alexey@gmail.com>
2024-12-10 20:15:40 +0300

    add loginfrom

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,6 +1,22 @@
 from django import forms
+from django.contrib.auth import get_user_model
 
 
 class LoginForm(forms.Form):
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label='Repeat password',
+        widget=forms.PasswordInput
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'email']
