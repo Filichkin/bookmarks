b4b85ff54156d193ab23955df28a0c4e9dbf0f56
Alexey Filichkin <filichkin.alexey@gmail.com>
2024-12-22 22:53:24 +0300

    like button activation

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -58,4 +58,4 @@ def image_like(request):
             return JsonResponse({'status': 'ok'})
         except Image.DoesNotExist:
             pass
-        return JsonResponse({'status': 'error'})
+    return JsonResponse({'status': 'error'})
