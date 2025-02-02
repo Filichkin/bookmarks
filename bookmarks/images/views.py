2b4dfa79bb49330f6af20b41ed401b92b53703e4
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-02-02 20:30:56 +0300

    add image_ranking

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -50,6 +50,7 @@ def image_create(request):
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     total_views = REDIS.incr(f'image:{image.id}:views')
+    REDIS.zincrby('image_ranking', 1, image.id)
     return render(
         request,
         'images/image/detail.html',
@@ -106,3 +107,26 @@ def image_list(request):
         'images/image/list.html',
         {'section': 'images', 'images': images}
         )
+
+
+@login_required
+def image_ranking(request):
+    image_ranking = REDIS.zrange(
+        'image_ranking', 0, -1,
+        desc=True
+    )
+    image_ranking_ids = [int(id) for id in image_ranking]
+    most_viewed = list(
+        Image.objects.filter(
+            id__in=image_ranking_ids
+        )
+    )
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(
+        request,
+        'images/image/ranking.html',
+        {
+            'section': 'images',
+            'most_viewed': most_viewed
+        }
+    )
